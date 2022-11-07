4bdcdf4e337012ffd3ad0e8716533c3efcf5e4d9
OTANASIIENKO-HP\otanasiienko <OlexandrTanasienko@gmail.com>
2022-11-07 17:19:16 +0200

    Write playlist controller

---
+++ backend/Melody.WebAPI/DTO/Playlist/UpdatePlaylistDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Melody.WebAPI.DTO.Playlist;
+
+public class UpdatePlaylistDto
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Link { get; set; }
+    public long AuthorId { get; set; }
+}
---
+++ backend/Melody.WebAPI/Validators/Playlist/UpdatePlaylistDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Melody.WebAPI.DTO.Playlist;
+
+namespace Melody.WebAPI.Validators.Playlist;
+
+public class UpdatePlaylistDtoValidator : AbstractValidator<UpdatePlaylistDto>
+{
+    public UpdatePlaylistDtoValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty()
+            .MaximumLength(50);
+
+        RuleFor(p => p.Link)
+            .NotEmpty()
+            .MaximumLength(50);
+    }
+}
