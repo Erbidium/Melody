cc0965dba8a0cbc386b42032e9f338620927c334
OTANASIIENKO-HP\otanasiienko <OlexandrTanasienko@gmail.com>
2022-11-07 17:00:15 +0200

    Write genre repository

---
+++ backend/Melody.Core/Interfaces/IGenreRepository.cs
@@ -0,0 +1,9 @@
+﻿using Melody.Core.Entities;
+
+namespace Melody.Core.Interfaces;
+
+public interface IGenreRepository
+{
+    Task<Genre?> GetById(long id);
+    Task<IReadOnlyCollection<Genre>> GetAll();
+}
---
+++ backend/Melody.Infrastructure/Data/Repositories/GenreRepository.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using Melody.Core.Entities;
+using Melody.Core.Interfaces;
+using Melody.Infrastructure.Data.Context;
+using Melody.Infrastructure.Data.Records;
+
+namespace Melody.Infrastructure.Data.Repositories;
+
+public class GenreRepository : IGenreRepository
+{
+    private readonly DapperContext _context;
+    public GenreRepository(DapperContext context)
+    {
+        _context = context;
+    }
+    public async Task<IReadOnlyCollection<Genre>> GetAll()
+    {
+        var query = "SELECT Id, Name FROM Genres";
+
+        using var connection = _context.CreateConnection();
+
+        var genres = await connection.QueryAsync<GenreRecord>(query);
+        return genres.Select(record => new Genre(record.Id, record.Name)).ToList().AsReadOnly();
+    }
+
+    public async Task<Genre?> GetById(long id)
+    {
+        var query = "SELECT Id, Name FROM Genres WHERE Id = @Id";
+
+        using var connection = _context.CreateConnection();
+
+        var record = await connection.QuerySingleOrDefaultAsync<GenreRecord>(query, new { id });
+        return record == null ? null : new Genre(record.Id, record.Name);
+    }
+}
