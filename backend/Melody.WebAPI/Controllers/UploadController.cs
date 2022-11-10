54fb45480a731e32e24d3eac671a41b5313d21f8
Oleksandr <OlexandrTanasienko@gmail.com>
2022-11-10 23:22:08 +0200

    Add current user to upload controller

--- backend/Melody.WebAPI/Controllers/UploadController.cs
+++ backend/Melody.WebAPI/Controllers/UploadController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Melody.Core.Entities;
+using Melody.Infrastructure.Data.Records;
+using Melody.Infrastructure.Data.Repositories;
+using Melody.WebAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System.IO;
+using System.Security.Claims;
 
 namespace Melody.WebAPI.Controllers
 {
@@ -7,17 +14,27 @@ namespace Melody.WebAPI.Controllers
     [ApiController]
     public class UploadController : ControllerBase
     {
+        private readonly ISongRepository _songRepository;
+        private readonly TokenService _tokenService;
+        private readonly UserManager<UserIdentity> _userManager;
+        public UploadController(ISongRepository songRepository, TokenService tokenService, UserManager<UserIdentity> userManager)
+        {
+            _songRepository = songRepository;
+            _tokenService = tokenService;
+            _userManager = userManager;
+        }
         private const string soundExtension = ".bmp";
         private const string folderName = "Sounds";
+        [Authorize]
         [HttpPost]
-        public IActionResult UploadFile(IFormFile uploadedSoundFile)
+        public async Task<IActionResult> UploadFile(IFormFile uploadedSoundFile)
         {
             // TODO: check total user uploads size by sql query by filtering isDeleted
             var extension = Path.GetExtension(uploadedSoundFile.FileName);
-            /*if (extension != soundExtension)
+            if (extension != soundExtension)
             {
                 return BadRequest();
-            }*/
+            }
 
             var currentDirectory = Directory.GetCurrentDirectory();
             var pathToSave = Path.Combine(currentDirectory, folderName);
@@ -45,6 +62,9 @@ namespace Melody.WebAPI.Controllers
             {
                 uploadedSoundFile.CopyTo(stream);
             }
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            var currentUserFromToken = _tokenService.GetCurrentUser(identity);
+            var user = await _userManager.FindByEmailAsync(currentUserFromToken.Email);
             // TODO: write information to db about user uploader, upload date, upload size with IsDeleted property
             return Ok(Path.Combine(folderName, guidSubFolders, guidFileName));
         }
