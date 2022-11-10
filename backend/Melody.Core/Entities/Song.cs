2090c4fdabd5ba004cd398e7845f775d29e00741
Oleksandr <OlexandrTanasienko@gmail.com>
2022-11-10 22:50:53 +0200

    Add  fields to songs

--- backend/Melody.Core/Entities/Song.cs
+++ backend/Melody.Core/Entities/Song.cs
@@ -5,14 +5,18 @@ namespace Melody.Core.Entities;
 
 public class Song : IEntityBase<long>
 {
-    public Song(string name, string path, string authorName, int year, long genreId, long id = -1)
+    public Song(long userId, string name, string generatedName, string path, string authorName, int year, long genreId, long sizeBytes, DateOnly uploadedAt, long id = -1)
     {
         Id = id;
+        UserId = userId;
         Name = name;
+        GeneratedName = generatedName;
         Path = path;
         AuthorName = authorName;
         Year = year;
         GenreId = genreId;
+        SizeBytes = sizeBytes;
+        UploadedAt = uploadedAt;
     }
 
     private long _id;
@@ -21,10 +25,13 @@ public class Song : IEntityBase<long>
         get => _id < 0 ? throw new WrongIdException() : _id;
         private set => _id = value;
     }
-        
+    public long UserId { get; }   
     public string Name { get; }
+    public string GeneratedName { get; }
     public string Path { get; }
     public string AuthorName { get; }
     public int Year { get; }
+    public long SizeBytes { get; }
+    public DateOnly UploadedAt { get; }
     public long GenreId { get; }
 }
--- backend/Melody.Infrastructure/Data/Repositories/SongRepository.cs
+++ backend/Melody.Infrastructure/Data/Repositories/SongRepository.cs
@@ -19,7 +19,7 @@ public class SongRepository : ISongRepository
         using var connection = _context.CreateConnection();
 
         var songs = await connection.QueryAsync<SongDb>(SqlScriptsResource.GetAllSongs);
-        return songs.Select(record => new Song(record.Name, record.Path, record.AuthorName, record.Year, record.GenreId, record.Id)).ToList().AsReadOnly();
+        return songs.Select(record => new Song(record.UserId, record.Name, record.GeneratedName, record.Path, record.AuthorName, record.Year, record.GenreId, record.SizeBytes, record.UploadedAt, record.Id)).ToList().AsReadOnly();
     }
 
     public async Task<Song?> GetById(long id)
@@ -27,32 +27,40 @@ public class SongRepository : ISongRepository
         using var connection = _context.CreateConnection();
 
         var record = await connection.QuerySingleOrDefaultAsync<SongDb>(SqlScriptsResource.GetSongById, new { id });
-        return record == null ? null : new Song(record.Name, record.Path, record.AuthorName, record.Year, record.GenreId, record.Id);
+        return record == null ? null : new Song(record.UserId, record.Name, record.GeneratedName, record.Path, record.AuthorName, record.Year, record.GenreId, record.SizeBytes, record.UploadedAt, record.Id);
     }
 
     public async Task<Song> Create(Song song)
     {
         var parameters = new DynamicParameters();
+        parameters.Add("UserId", song.UserId, DbType.Int64);
         parameters.Add("Name", song.Name, DbType.String);
+        parameters.Add("GeneratedName", song.GeneratedName, DbType.String);
         parameters.Add("Path", song.Path, DbType.String);
         parameters.Add("AuthorName", song.AuthorName, DbType.String);
         parameters.Add("Year", song.Year, DbType.Int32);
+        parameters.Add("SizeBytes", song.SizeBytes, DbType.Int32);
+        parameters.Add("UploadedAt", song.UploadedAt, DbType.Date);
         parameters.Add("GenreId", song.GenreId, DbType.Int64);
 
         using var connection = _context.CreateConnection();
 
         var id = await connection.ExecuteScalarAsync<long>(SqlScriptsResource.CreateSong, parameters);
 
-        return new Song(song.Name, song.Path, song.AuthorName, song.Year, song.GenreId, id);
+        return new Song(song.UserId, song.Name, song.GeneratedName, song.Path, song.AuthorName, song.Year, song.GenreId, song.SizeBytes, song.UploadedAt, id);
     }
 
     public async Task Update(Song song)
     {
         var parameters = new DynamicParameters();
+        parameters.Add("UserId", song.UserId, DbType.Int64);
         parameters.Add("Name", song.Name, DbType.String);
+        parameters.Add("GeneratedName", song.GeneratedName, DbType.String);
         parameters.Add("Path", song.Path, DbType.String);
         parameters.Add("AuthorName", song.AuthorName, DbType.String);
         parameters.Add("Year", song.Year, DbType.Int32);
+        parameters.Add("SizeBytes", song.SizeBytes, DbType.Int32);
+        parameters.Add("UploadedAt", song.UploadedAt, DbType.Date);
         parameters.Add("GenreId", song.GenreId, DbType.Int64);
 
         using var connection = _context.CreateConnection();
