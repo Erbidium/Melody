91bcc8b2dd89362f5fcc7dd475c29ab267d4362e
Oleksandr <OlexandrTanasienko@gmail.com>
2022-11-18 15:55:48 +0200

    Add http internal service for requests

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '@shared/shared.module';
 
 import { BaseComponent } from './base/base.component';
 
 @NgModule({
-    declarations: [
-        BaseComponent,
-    ],
     imports: [
         CommonModule,
+        SharedModule,
+    ],
+    declarations: [
+        BaseComponent,
     ],
 })
 export class CoreModule { }
