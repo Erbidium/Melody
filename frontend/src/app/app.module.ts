b0455fb225f874879cb9634984d83bd473040c08
Oleksandr <OlexandrTanasienko@gmail.com>
2022-11-21 22:16:52 +0200

    Import animations module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { SharedModule } from '@shared/shared.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from "@shared/material/material.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, SharedModule, MaterialModule],
+    imports: [BrowserModule, AppRoutingModule, SharedModule, MaterialModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
