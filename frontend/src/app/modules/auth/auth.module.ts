c977d615546b72d9a64a25907568e6707ff7acbc
Oleksandr <OlexandrTanasienko@gmail.com>
2022-11-23 17:39:45 +0200

    Fix melody page routing

--- frontend/src/app/modules/auth/auth.module.ts
+++ frontend/src/app/modules/auth/auth.module.ts
@@ -1,10 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatTabsModule } from '@angular/material/tabs';
 import { AuthRoutingModule } from '@modules/auth/auth-routing.module';
 import { MaterialModule } from '@shared/material/material.module';
 import { SharedModule } from '@shared/shared.module';
--- frontend/src/app/modules/melody/melody.module.ts
+++ frontend/src/app/modules/melody/melody.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MelodyPageComponent } from '@modules/melody/melody-page/melody-page.component';
+import { MelodyRoutingModule } from '@modules/melody/melody-routing.module';
 
 @NgModule({
     declarations: [
@@ -8,6 +9,7 @@ import { MelodyPageComponent } from '@modules/melody/melody-page/melody-page.com
     ],
     imports: [
         CommonModule,
+        MelodyRoutingModule,
     ],
 })
 export class MelodyModule { }
