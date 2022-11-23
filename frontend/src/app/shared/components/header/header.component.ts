9b7fd2a0808e5e95a905699f1be9841f03dd1c3d
Oleksandr <OlexandrTanasienko@gmail.com>
2022-11-23 17:57:41 +0200

    Navigate to profile page

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { BaseComponent } from '@core/base/base.component';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.sass'],
 })
-export class HeaderComponent {
+export class HeaderComponent extends BaseComponent {
     public navLinks = [
         { path: '/melody', label: 'Main' },
         { path: '/recommendations', label: 'Recommendations' },
         { path: '/statistics', label: 'Statistics' },
         { path: '/upload', label: 'Load song' },
     ];
+
+    constructor(
+        private router: Router,
+    ) {
+        super();
+    }
+
+    public navigateToProfile() {
+        this.router.navigateByUrl(
+            '/profile',
+        );
+    }
 }
