bb66a1a322c474daf7d01ec93c6b3d05ffb47355
Oleksandr <OlexandrTanasienko@gmail.com>
2022-11-23 19:12:52 +0200

    Add basic profile page layout

--- frontend/src/app/modules/profile/profile.module.ts
+++ frontend/src/app/modules/profile/profile.module.ts
@@ -4,6 +4,7 @@ import { ProfileRoutingModule } from '@modules/profile/profile-routing.module';
 import { SharedModule } from '@shared/shared.module';
 
 import { ProfilePageComponent } from './profile-page/profile-page.component';
+import {MaterialModule} from "@shared/material/material.module";
 
 @NgModule({
     declarations: [
@@ -13,6 +14,7 @@ import { ProfilePageComponent } from './profile-page/profile-page.component';
         CommonModule,
         ProfileRoutingModule,
         SharedModule,
+        MaterialModule,
     ],
 })
 export class ProfileModule { }
