c3d5778a1539fbd3c4976b90b298599c785ef0d8
Oleksandr <OlexandrTanasienko@gmail.com>
2022-11-23 19:36:46 +0200

    Add sign out from profile page

--- frontend/src/app/modules/profile/profile-page/profile-page.component.ts
+++ frontend/src/app/modules/profile/profile-page/profile-page.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 import { BaseComponent } from '@core/base/base.component';
+import {AuthService} from "@core/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-profile-page',
     templateUrl: './profile-page.component.html',
     styleUrls: ['./profile-page.component.sass'],
 })
-export class ProfilePageComponent extends BaseComponent { }
+export class ProfilePageComponent extends BaseComponent {
+    constructor(private authService: AuthService, private router: Router) {
+        super();
+    }
+
+    logOut() {
+        this.authService.signOut();
+        this.router.navigateByUrl('auth');
+    }
+}
