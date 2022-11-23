43d0dd59407f82f840c1b4c6f92553f96e2baa42
Oleksandr <OlexandrTanasienko@gmail.com>
2022-11-23 12:40:49 +0200

    Add Auth guard

--- frontend/src/app/modules/upload/upload-routing-module.ts
+++ frontend/src/app/modules/upload/upload-routing-module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@core/guards/auth.guard';
 import { UploadPageComponent } from '@modules/upload/upload-page/upload-page.component';
 
 const routes: Routes = [
     {
         path: '',
         component: UploadPageComponent,
+        canActivate: [AuthGuard],
     },
     {
         path: '**',
