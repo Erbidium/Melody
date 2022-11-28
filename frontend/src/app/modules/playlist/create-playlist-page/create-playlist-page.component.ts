628ff803f2e13a5a4f2947081c330ff735e12f53
Oleksandr <OlexandrTanasienko@gmail.com>
2022-11-28 12:57:30 +0200

    Create page for playlist creation

--- frontend/src/app/modules/playlist/create-playlist-page/create-playlist-page.component.ts
+++ frontend/src/app/modules/playlist/create-playlist-page/create-playlist-page.component.ts
@@ -1,13 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '@core/base/base.component';
+import { ISong } from '@core/models/ISong';
+import { SongService } from '@core/services/song.service';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
     selector: 'app-create-playlist-page',
     templateUrl: './create-playlist-page.component.html',
     styleUrls: ['./create-playlist-page.component.sass'],
 })
-export class CreatePlaylistPageComponent implements OnInit {
-    constructor() { }
+export class CreatePlaylistPageComponent extends BaseComponent implements OnInit {
+    uploadedSongs: ISong[] = [];
+
+    public createPlaylistForm = new FormGroup({
+        name: new FormControl('', {
+            validators: [Validators.required, Validators.minLength(8), Validators.maxLength(30)],
+            updateOn: 'blur',
+        }),
+        songs: new FormControl('', {
+            validators: [Validators.required],
+            updateOn: 'blur',
+        }),
+    });
+
+    constructor(private songService: SongService) {
+        super();
+    }
 
     ngOnInit(): void {
+        this.loadSongsUploadedByUser();
+    }
+
+    loadSongsUploadedByUser() {
+        this.songService
+            .getSongsUploadedByUser()
+            .pipe(this.untilThis)
+            .subscribe((resp) => {
+                this.uploadedSongs = resp;
+            });
+    }
+
+    create() {
+        console.log(this.createPlaylistForm.value.songs);
     }
 }
--- frontend/src/app/modules/playlist/playlist.module.ts
+++ frontend/src/app/modules/playlist/playlist.module.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PlaylistRoutingModule } from '@modules/playlist/playlist-routing.module';
 import { CreatePlaylistPageComponent } from './create-playlist-page/create-playlist-page.component';
+import {MaterialModule} from "@shared/material/material.module";
+import {ReactiveFormsModule} from "@angular/forms";
+import {SharedModule} from "@shared/shared.module";
 
 @NgModule({
     declarations: [
@@ -10,6 +13,9 @@ import { CreatePlaylistPageComponent } from './create-playlist-page/create-playl
     imports: [
         CommonModule,
         PlaylistRoutingModule,
+        MaterialModule,
+        ReactiveFormsModule,
+        SharedModule,
     ],
 })
 export class PlaylistModule { }
