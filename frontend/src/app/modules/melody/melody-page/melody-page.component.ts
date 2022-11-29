a08ef8822ba266ba956ff6cc3e13c51a16ffe85c
Oleksandr <OlexandrTanasienko@gmail.com>
2022-11-29 22:03:33 +0200

    Show user playlists on the melody page

--- frontend/src/app/modules/melody/melody-page/melody-page.component.ts
+++ frontend/src/app/modules/melody/melody-page/melody-page.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BaseComponent } from '@core/base/base.component';
+import { IPlaylistWithPerformers } from '@core/models/IPlaylistWithPerformers';
+import { PlaylistService } from '@core/services/playlist.service';
 
 @Component({
     selector: 'app-melody-page',
     templateUrl: './melody-page.component.html',
     styleUrls: ['./melody-page.component.sass'],
 })
-export class MelodyPageComponent extends BaseComponent {
-    constructor(private router: Router) {
+export class MelodyPageComponent extends BaseComponent implements OnInit {
+    userPlaylists: IPlaylistWithPerformers[] = [];
+
+    constructor(
+        private playlistService: PlaylistService,
+        private router: Router,
+    ) {
         super();
+    }
+
+    ngOnInit(): void {
+        this.playlistService
+            .getPlaylistsCreatedByUser()
+            .pipe(this.untilThis)
+            .subscribe((playlists) => {
+                this.userPlaylists = playlists;
+            });
     }
 
     createPlaylist() {
